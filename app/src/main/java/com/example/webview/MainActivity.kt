d10c3f86d09f7aaa90a48636c987ed4571e9060a
Almuheetu shihab <almuheetu@gmail.com>
2024-11-21 12:13:56 +0600

    first coponents working on going

--- app/src/main/java/com/example/webview/MainActivity.kt
+++ app/src/main/java/com/example/webview/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.webview.ui.components.WebViewPage
 import com.example.webview.ui.theme.WebVIewTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,7 +19,14 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-
+            WebVIewTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    WebViewPage(
+                        url = "https://www.google.com",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
         }
     }
 }
--- app/src/main/java/com/example/webview/ui/components/WebView.kt
+++ app/src/main/java/com/example/webview/ui/components/WebView.kt
@@ -3,23 +3,33 @@ package com.example.webview.ui.components
 import android.view.ViewGroup
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 
 @Composable
-fun WebViewPage(url: String) {
+fun WebViewPage(url: String, modifier: Modifier = Modifier) {
 
-    AndroidView(factory = {
-        WebView(it).apply {
-            layoutParams = ViewGroup.LayoutParams(
-                ViewGroup.LayoutParams.MATCH_PARENT,
-                ViewGroup.LayoutParams.MATCH_PARENT
-            )
-            webViewClient = WebViewClient()
-            loadUrl(url)
-        }
-    },
+    AndroidView(
+        factory = {
+            WebView(it).apply {
+                layoutParams = ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.MATCH_PARENT
+                )
+                webViewClient = WebViewClient()
+                loadUrl(url)
+            }
+        },
         update = {
             it.loadUrl(url)
-        })
+        },
+        modifier = modifier.padding(
+            top = 25.dp,
+            start = 10.dp,
+            end = 10.dp
+        )
+    )
 }
