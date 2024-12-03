1259ced83a13b70c7184bbfdf587ba7ddbac4130
Almuheetu shihab <almuheetu@gmail.com>
2024-12-03 17:10:00 +0600

    not found screen making complete

--- app/src/main/java/com/example/webview/ui/screen/infocardscreen/component/NotFoundScreen.kt
+++ app/src/main/java/com/example/webview/ui/screen/infocardscreen/component/NotFoundScreen.kt
@@ -1,2 +1,75 @@
 package com.example.webview.ui.screen.infocardscreen.component
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.ShoppingCart
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.webview.ui.components.PreviewAppScreen
+
+@Composable
+fun NotFoundScreen(
+    icon: ImageVector,
+    title: String,
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Box(
+            modifier = Modifier
+                .size(100.dp)
+                .clip(CircleShape)
+                .background(
+                    color = MaterialTheme.colorScheme.primary.copy(.1f)
+                ),
+            contentAlignment = Alignment.Center
+        ) {
+            Icon(
+                imageVector = icon,
+                contentDescription = "",
+                tint = MaterialTheme.colorScheme.primary.copy(alpha = 0.5f),
+                modifier = Modifier.size((100 * 0.5).dp)
+            )
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Text(
+            text = title,
+            style = MaterialTheme.typography.titleSmall.copy(
+                fontSize = 20.sp,
+                color = MaterialTheme.colorScheme.onSurface
+            )
+        )
+    }
+}
+
+@PreviewAppScreen
+@Composable
+fun NotFoundScreenPreview() {
+    NotFoundScreen(
+        icon = Icons.Outlined.ShoppingCart,
+        title = "404 Not Found",
+    )
+}
